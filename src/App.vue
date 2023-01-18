521113ebada2af9586addff679fcaede64884f9d
Manoj0718 <flyindessert@gmail.com>
2023-01-18 17:02:35 +0100

    done frontend

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
     <WhatSection></WhatSection>
     <GetRequest></GetRequest>
     <PostRequest></PostRequest>
-    <!-- <whySection></whySection> -->
+    <WhySection></WhySection>
     <Footer></Footer>
   </div>
 </template>
@@ -16,12 +16,12 @@ import Header from './components/Header.vue';
 import PostRequest from './components/POST.vue';
 import WhatSection from './components/WhatSection.vue';
 import Footer from './components/Footer.vue';
-// import whySection from './components/whySection.vue';
+import WhySection from './components/whySection.vue';
 import HeroVue from './components/Hero.vue';
 
 export default {
 
-  components: { GetRequest, Header, PostRequest, WhatSection, Footer, HeroVue }
+  components: { Header, HeroVue, WhatSection, GetRequest, PostRequest, WhySection, Footer }
 }
 </script>
 
